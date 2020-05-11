2d7a3f8ffeb0d223251ce19166cd29bb36aa825e
chattyy <55800397+chattyy@users.noreply.github.com>
2020-05-11 19:08:01 +0530

    Update Plot1.r

--- Plot1.r
+++ Plot1.r
@@ -4,6 +4,6 @@ subsetdata <- ucw[ucw$Date %in% c("1/2/2007","2/2/2007") ,]
 
 
 GlobalActivePower <- as.numeric(subsetdata$Global_active_power)
-png("plot1.png", width=600, height=600)
+png("plot1.png", width=480, height=480)
 hist(GlobalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 
